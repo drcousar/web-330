f83bc221a1483e4f4be48d94362799758b0667ab
Donald Cousar <drcousar@yahoo.com>
2019-02-23 21:51:13 -0600

    Add Assignment 1.3

--- week-1/cousar-assignment-1.3.js
+++ week-1/cousar-assignment-1.3.js
@@ -4,13 +4,53 @@
 ; Author: Don Cousar
 ; Date:   23 Feb 2019
 ; Description: This program demonstrates the
-;   use of Modules
+;   use of Classes
 ;===========================================
-*/ 
-var url = require("url");
 
-var parsedURL = url.parse("https://www.google.com/profile?name=cousar");
+ Expected output:
 
-console.log(parsedURL.protocol);
-console.log(parsedURL.host);
-console.log(parsedURL.query);
+ FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+
+ -- DISPLAYING CELL PHONE DETAILS --
+ Manufacturer: <manufacturer>
+ Model: <model>
+ Color: <color>
+ Price: <price>
+
+*/
+
+// start program
+
+//Call Header Info
+var header = require('../cousar-header');
+console.log(header.display("Donald", "Cousar","Exersize 1.3"));
+console.log('\n');
+
+//Build Object
+function CellPhone(manufacturer, model, color, price) {
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.color = color;
+    this.price = price;
+
+    this.getPrice = function() {
+        return this.price;
+    };
+    
+    this.getModel = function() {
+        return this.model;
+    };
+
+    this.getDetails = function() {
+        return "Manufacturer: " + this.manufacturer + "\nModel: " + this.getModel() + "\nColor: " + this.color + "\nPrice: " + this.getPrice();
+    };
+    
+}
+    //Define and print cellphone details
+    var cellphone = new CellPhone("Apple", "iPhone XS Max", "Black", "$1099.00");
+    console.log(" -- DISPLAYING CELL PHONE DETAILS --")
+    console.log(cellphone.getDetails());
+
+// end program
